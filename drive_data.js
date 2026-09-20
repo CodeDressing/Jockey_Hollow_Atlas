e0c86e10966aadadc94be265cf5d7e4e49d07750
Ryan M Schuren <ryanschuren@gmail.com>
2026-09-20 01:32:19 -0400

    Add AN2-F2 Rutgers Drive image series to MycoScope

--- drive_data.js
+++ drive_data.js
@@ -1,1 +1,67 @@
-window.DRIVE_PHOTOS = [];
+window.DRIVE_PHOTOS = [
+  {
+    "folder": "AN2 2026",
+    "collection": "AN2",
+    "family": "AN",
+    "specimen": "AN2-F2",
+    "category": "Source macro / specimen",
+    "filename": "AN2-F2.HEIC",
+    "caption": "AN2-F2 — additional specimen view from Rutgers Drive",
+    "original": "https://drive.google.com/thumbnail?id=1Y6u6OeQHtW6TmPG39pl3YNXuTxCH7mxx&sz=w2400",
+    "thumb": "https://drive.google.com/thumbnail?id=1Y6u6OeQHtW6TmPG39pl3YNXuTxCH7mxx&sz=w1200",
+    "source_relpath": "AN2 2026/AN2-F2.HEIC",
+    "drive_view": "https://drive.google.com/file/d/1Y6u6OeQHtW6TmPG39pl3YNXuTxCH7mxx/view"
+  },
+  {
+    "folder": "AN2 2026",
+    "collection": "AN2",
+    "family": "AN",
+    "specimen": "AN2-F2",
+    "category": "Source macro / specimen",
+    "filename": "AN2-F2V3.HEIC",
+    "caption": "AN2-F2 — additional hymenophore/specimen view V3 from Rutgers Drive",
+    "original": "https://drive.google.com/thumbnail?id=1DrJNrzuDTn0gGG5DDSefjZ0Cigf8r1Pt&sz=w2400",
+    "thumb": "https://drive.google.com/thumbnail?id=1DrJNrzuDTn0gGG5DDSefjZ0Cigf8r1Pt&sz=w1200",
+    "source_relpath": "AN2 2026/AN2-F2V3.HEIC",
+    "drive_view": "https://drive.google.com/file/d/1DrJNrzuDTn0gGG5DDSefjZ0Cigf8r1Pt/view"
+  },
+  {
+    "folder": "AN2 2026",
+    "collection": "AN2",
+    "family": "AN",
+    "specimen": "AN2-F2",
+    "category": "Source macro / specimen",
+    "filename": "AN2-F2V4.HEIC",
+    "caption": "AN2-F2 — additional specimen view V4 from Rutgers Drive",
+    "original": "https://drive.google.com/thumbnail?id=1HT2i2nUMiu-nSuTt1WRujZ3Ire5IaaTW&sz=w2400",
+    "thumb": "https://drive.google.com/thumbnail?id=1HT2i2nUMiu-nSuTt1WRujZ3Ire5IaaTW&sz=w1200",
+    "source_relpath": "AN2 2026/AN2-F2V4.HEIC",
+    "drive_view": "https://drive.google.com/file/d/1HT2i2nUMiu-nSuTt1WRujZ3Ire5IaaTW/view"
+  },
+  {
+    "folder": "AN2 2026",
+    "collection": "AN2",
+    "family": "AN",
+    "specimen": "AN2-F2",
+    "category": "Source macro / specimen",
+    "filename": "AN2-F2V5.HEIC",
+    "caption": "AN2-F2 — additional specimen view V5 from Rutgers Drive",
+    "original": "https://drive.google.com/thumbnail?id=1PdTwnbqTH3jR6PC4pemLybuS--YwxQTK&sz=w2400",
+    "thumb": "https://drive.google.com/thumbnail?id=1PdTwnbqTH3jR6PC4pemLybuS--YwxQTK&sz=w1200",
+    "source_relpath": "AN2 2026/AN2-F2V5.HEIC",
+    "drive_view": "https://drive.google.com/file/d/1PdTwnbqTH3jR6PC4pemLybuS--YwxQTK/view"
+  },
+  {
+    "folder": "AN2 2026",
+    "collection": "AN2",
+    "family": "AN",
+    "specimen": "AN2-F2",
+    "category": "Source macro / specimen",
+    "filename": "AN2-F2V6.HEIC",
+    "caption": "AN2-F2 — additional specimen view V6 from Rutgers Drive",
+    "original": "https://drive.google.com/thumbnail?id=1NIhboK01Lm2q5ogGSVhCOIWRwYmPK-5v&sz=w2400",
+    "thumb": "https://drive.google.com/thumbnail?id=1NIhboK01Lm2q5ogGSVhCOIWRwYmPK-5v&sz=w1200",
+    "source_relpath": "AN2 2026/AN2-F2V6.HEIC",
+    "drive_view": "https://drive.google.com/file/d/1NIhboK01Lm2q5ogGSVhCOIWRwYmPK-5v/view"
+  }
+];
